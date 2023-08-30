5462e163f5dce47c8dc3dc7388601c1fc1fe1584
Ajay <ajaydeep365@gmail.com>
2023-08-30 08:40:45 +0530

    first commit

---
+++ src/main/java/com/example/demo/Controller/MyController.java
@@ -0,0 +1,25 @@
+package com.example.demo.Controller;
+
+import com.example.demo.models.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/student")
+public class MyController {
+    @Autowired
+    private StudentRepository studentRepository;
+    @PostMapping("/")
+    public ResponseEntity<?> addStudent(@RequestBody Student student){
+        Student save = this.studentRepository.save(student);
+        return ResponseEntity.ok(save);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> getStudent(){
+
+        return ResponseEntity.ok(this.studentRepository.findAll());
+    }
+}
